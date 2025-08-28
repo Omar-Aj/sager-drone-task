68b7ab8e95b011986cff5844f0e0107f3150c30a
Omar Ajaj <omar.aj.business@gmail.com>
2025-08-28 23:20:29 +0300

    Added message to dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <p className="animate-gradient bg-gradient-to-r from-red-500 via-yellow-500 to-orange-500 bg-clip-text text-7xl font-bold text-transparent md:text-9xl lg:text-[256px]">
+      <p className="bg-gradient-to-r from-red-500 via-yellow-500 to-orange-500 bg-clip-text text-7xl font-bold text-transparent md:text-9xl lg:text-[256px]">
         Hello
       </p>
-      <p className="animate-gradient bg-gradient-to-r from-red-500 via-yellow-500 to-orange-500 bg-clip-text text-7xl font-bold text-transparent md:text-9xl lg:text-[256px]">
+      <p className="bg-gradient-to-r from-red-500 via-yellow-500 to-orange-500 bg-clip-text text-7xl font-bold text-transparent md:text-9xl lg:text-[256px]">
         {":>"}
       </p>
     </div>
